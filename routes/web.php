fe79d5fc753b2316685066d41ce52d089a0c3168
gssa-13 <gssa-13@protonmail.com>
2021-12-27 21:44:44 -0600

    Estatuses
    * Created Model, migration, controller
    * Created some validation test to create status

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\StatusController;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::post('statuses', [StatusController::class, 'store'])->name('statuses.store')->middleware('auth');
+
+Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login');
+Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])->name('logout');
---
+++ tests/Feature/CreateStatusTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CreateStatusTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function an_authenticated_user_can_create_statuses()
+    {
+        $this->withoutExceptionHandling();
+        // 1 given => Getting an authenticated user
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        // 2 When => Make a post request to status
+        $response = $this->postJson(route('statuses.store'), ['body' => 'My first post']);
+        $response->assertJson([
+            'body' => 'My first post'
+        ]);
+        // 3 Then => See a new status from database
+        $this->assertDatabaseHas('statuses', [
+            'user_id' => $user->id,
+            'body' => 'My first post'
+        ]);
+    }
+
+    /** @test */
+    public function guests_can_not_create_statuses()
+    {
+        $response = $this->post(route('statuses.store'), ['body' => 'My first post']);
+        $response->assertRedirect('login');
+    }
+
+    /** @test */
+    public function a_status_require_a_body()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), ['body' => '']);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
+        ]);
+    }
+
+    /** @test */
+    public function a_status_requires_a_minimun_length()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $response = $this->postJson(route('statuses.store'), ['body' => 'qwer']);
+
+        $response->assertStatus(422);
+
+        $response->assertJsonStructure([
+            'message', 'errors' => ['body']
+        ]);
+    }
+}
